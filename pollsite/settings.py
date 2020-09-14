d859b3020fbcee98b546b5960faf66aeba5607c1
sharknoise <sharknoise@gmail.com>
2020-09-14 23:55:56 +0300

    revert to using sqlite

--- pollsite/settings.py
+++ pollsite/settings.py
@@ -77,12 +77,8 @@ WSGI_APPLICATION = 'pollsite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'pollsite',
-        'USER': 'pollmaster',
-        'PASSWORD': 'pollpassword',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
