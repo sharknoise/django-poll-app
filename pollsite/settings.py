cebe7fde44ae3c52fada131de8646a2e60ca17f6
sharknoise <sharknoise@gmail.com>
2020-09-16 00:40:47 +0300

    update static settings

--- pollsite/settings.py
+++ pollsite/settings.py
@@ -125,8 +125,8 @@ USE_TZ = True
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-#STATICFILES_DIRS = [
-#    os.path.join(BASE_DIR, 'polls/static/polls'),
-#]
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
