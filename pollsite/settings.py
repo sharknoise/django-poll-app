f99e6d5eb502631cf3377486edfddb4be25cb341
sharknoise <sharknoise@gmail.com>
2020-09-15 20:07:56 +0300

    update the debug setting for heroku

--- pollsite/settings.py
+++ pollsite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '=j6brtlwja##kbd2fe5a5ry6z$ubvdira-w_8k8*ua1tg%3wm0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '.localhost',
